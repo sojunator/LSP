d4c8a9e057c500fafe8518919b2363eab749543f
NickWedzinga <oxay@live.com>
2017-01-20 17:00:35 +0100

    Added getters/setters

--- THOMAS/Camera.cpp
+++ THOMAS/Camera.cpp
@@ -3,11 +3,16 @@
 
 thomas::Camera::Camera(DirectX::XMVECTOR camPos, DirectX::XMVECTOR focusAt, float fov, float viewNear, float viewFar)
 {
-	CreateViewMatrix(camPos, focusAt);
-	CreateProjMatrix(fov, viewNear, viewFar);
+	m_camPos = camPos;
+	m_focusAt = focusAt;
+	m_fov = fov;
+	m_near = viewNear;
+	m_far = viewFar;
+
+	Update(m_camPos, m_focusAt, m_fov, m_near,m_far);
 }
 
-DirectX::XMMATRIX thomas::Camera::CreateViewMatrix(DirectX::XMVECTOR camPos, DirectX::XMVECTOR focusAt)
+void thomas::Camera::CreateViewMatrix(DirectX::XMVECTOR camPos, DirectX::XMVECTOR focusAt)
 {
 	DirectX::XMVECTOR lookVector = DirectX::XMVectorSubtract(focusAt, camPos);
 	DirectX::XMVECTOR tempUp = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
@@ -17,14 +22,21 @@ DirectX::XMMATRIX thomas::Camera::CreateViewMatrix(DirectX::XMVECTOR camPos, Dir
 	m_viewMatrix = DirectX::XMMatrixLookAtLH(camPos, focusAt, up);
 }
 
-DirectX::XMMATRIX thomas::Camera::CreateProjMatrix(float fov, float viewNear, float viewFar)
+void thomas::Camera::CreateProjMatrix(float fov, float viewNear, float viewFar)
 {
 	m_projMatrix =  DirectX::XMMatrixPerspectiveFovLH(fov, Window::GetWidth() / Window::GetHeight(), viewNear, viewFar);
 }
 
-DirectX::XMMATRIX thomas::Camera::CreateViewProjMatrix()
+void thomas::Camera::CreateViewProjMatrix()
 {
 	m_viewProjMatrix = DirectX::XMMatrixMultiply(m_viewMatrix, m_projMatrix);
+}
+
+void thomas::Camera::Update(DirectX::XMVECTOR camPos, DirectX::XMVECTOR focusAt, float fov, float viewNear, float viewFar)
+{
+	CreateViewMatrix(m_camPos, m_focusAt);
+	CreateProjMatrix(m_fov, m_near, m_far);
+	CreateViewProjMatrix();
 }
 
 DirectX::XMMATRIX thomas::Camera::GetViewMatrix()
@@ -40,4 +52,64 @@ DirectX::XMMATRIX thomas::Camera::GetProjMatrix()
 DirectX::XMMATRIX thomas::Camera::GetViewProjMatrix()
 {
 	return m_viewProjMatrix;
+}
+
+DirectX::XMVECTOR thomas::Camera::GetCamPos()
+{
+	return m_camPos;
+}
+
+DirectX::XMVECTOR thomas::Camera::GetFocus()
+{
+	return m_focusAt;
+}
+
+float thomas::Camera::GetFov()
+{
+	return m_fov;
+}
+
+float thomas::Camera::GetNear()
+{
+	return m_near;
+}
+
+float thomas::Camera::GetFar()
+{
+	return m_far;
+}
+
+void thomas::Camera::SetCamPos(DirectX::XMVECTOR camPos)
+{
+	m_camPos = camPos;
+	CreateViewMatrix(m_camPos, m_focusAt);
+	CreateViewProjMatrix();
+}
+
+void thomas::Camera::SetFocus(DirectX::XMVECTOR focusAt)
+{
+	m_focusAt = focusAt;
+	CreateViewMatrix(m_camPos, m_focusAt);
+	CreateViewProjMatrix();
+}
+
+void thomas::Camera::SetFov(float fov)
+{
+	m_fov = fov;
+	CreateProjMatrix(m_fov, m_near, m_far);
+	CreateViewProjMatrix();
+}
+
+void thomas::Camera::SetNear(float viewNear)
+{
+	m_near = viewNear;
+	CreateProjMatrix(m_fov, m_near, m_far);
+	CreateViewProjMatrix();
+}
+
+void thomas::Camera::SetFar(float viewFar)
+{
+	m_far = viewFar;
+	CreateProjMatrix(m_fov, m_near, m_far);
+	CreateViewProjMatrix();
 }
--- THOMAS/Camera.h
+++ THOMAS/Camera.h
@@ -7,19 +7,35 @@ namespace thomas
 	class THOMAS_API Camera
 	{
 	private:
-
+		void CreateViewMatrix(DirectX::XMVECTOR camPos, DirectX::XMVECTOR focusAt);
+		void CreateProjMatrix(float fov, float near, float far);
+		void CreateViewProjMatrix();
+		void Update(DirectX::XMVECTOR camPos, DirectX::XMVECTOR focusAt, float fov, float viewNear, float viewFar);
+		
 	public:
 		Camera(DirectX::XMVECTOR camPos, DirectX::XMVECTOR focusAt, float fov, float near, float far);
-		DirectX::XMMATRIX CreateViewMatrix(DirectX::XMVECTOR camPos, DirectX::XMVECTOR focusAt);
-		DirectX::XMMATRIX CreateProjMatrix(float fov, float near, float far);
-		DirectX::XMMATRIX CreateViewProjMatrix();
 		DirectX::XMMATRIX GetViewMatrix();
 		DirectX::XMMATRIX GetProjMatrix();
 		DirectX::XMMATRIX GetViewProjMatrix();
+		DirectX::XMVECTOR GetCamPos();
+		DirectX::XMVECTOR GetFocus();
+		float GetFov();
+		float GetNear();
+		float GetFar();
+		void SetCamPos(DirectX::XMVECTOR camPos);
+		void SetFocus(DirectX::XMVECTOR focusAt);
+		void SetFov(float fov);
+		void SetNear(float viewNear);
+		void SetFar(float viewFar);
 
 	private:
-		DirectX::XMMATRIX m_viewMatrix; //ska det vara s_?
-		DirectX::XMMATRIX m_projMatrix; //ska det vara s_?
-		DirectX::XMMATRIX m_viewProjMatrix; //ska det vara s_?
+		DirectX::XMMATRIX m_viewMatrix;
+		DirectX::XMMATRIX m_projMatrix;
+		DirectX::XMMATRIX m_viewProjMatrix;
+		DirectX::XMVECTOR m_camPos;
+		DirectX::XMVECTOR m_focusAt;
+		float m_fov;
+		float m_near;
+		float m_far;
 	};
 }
