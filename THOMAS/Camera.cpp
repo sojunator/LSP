cea7828a497548e00f70e22d9e498044fff19a16
NickWedzinga <oxay@live.com>
2017-01-20 16:25:44 +0100

    Added ViewProj and Getters

--- THOMAS/Camera.cpp
+++ THOMAS/Camera.cpp
@@ -3,6 +3,7 @@
 
 thomas::Camera::Camera(DirectX::XMVECTOR camPos, DirectX::XMVECTOR focusAt, float fov, float viewNear, float viewFar)
 {
+	CreateViewMatrix(camPos, focusAt);
 	CreateProjMatrix(fov, viewNear, viewFar);
 }
 
@@ -13,11 +14,30 @@ DirectX::XMMATRIX thomas::Camera::CreateViewMatrix(DirectX::XMVECTOR camPos, Dir
 	DirectX::XMVECTOR right = DirectX::XMVector3Cross(tempUp, lookVector);
 	DirectX::XMVECTOR up =  DirectX::XMVector3Cross(lookVector, right);
 
-	return DirectX::XMMatrixLookAtLH(camPos, focusAt, up);
+	m_viewMatrix = DirectX::XMMatrixLookAtLH(camPos, focusAt, up);
 }
 
 DirectX::XMMATRIX thomas::Camera::CreateProjMatrix(float fov, float viewNear, float viewFar)
 {
-	return DirectX::XMMatrixPerspectiveFovLH(fov, Window::GetWidth() / Window::GetHeight(), viewNear, viewFar);
+	m_projMatrix =  DirectX::XMMatrixPerspectiveFovLH(fov, Window::GetWidth() / Window::GetHeight(), viewNear, viewFar);
+}
 
+DirectX::XMMATRIX thomas::Camera::CreateViewProjMatrix()
+{
+	m_viewProjMatrix = DirectX::XMMatrixMultiply(m_viewMatrix, m_projMatrix);
+}
+
+DirectX::XMMATRIX thomas::Camera::GetViewMatrix()
+{
+	return m_viewMatrix;
+}
+
+DirectX::XMMATRIX thomas::Camera::GetProjMatrix()
+{
+	return m_projMatrix;
+}
+
+DirectX::XMMATRIX thomas::Camera::GetViewProjMatrix()
+{
+	return m_viewProjMatrix;
 }
--- THOMAS/Camera.h
+++ THOMAS/Camera.h
@@ -12,7 +12,14 @@ namespace thomas
 		Camera(DirectX::XMVECTOR camPos, DirectX::XMVECTOR focusAt, float fov, float near, float far);
 		DirectX::XMMATRIX CreateViewMatrix(DirectX::XMVECTOR camPos, DirectX::XMVECTOR focusAt);
 		DirectX::XMMATRIX CreateProjMatrix(float fov, float near, float far);
+		DirectX::XMMATRIX CreateViewProjMatrix();
+		DirectX::XMMATRIX GetViewMatrix();
+		DirectX::XMMATRIX GetProjMatrix();
+		DirectX::XMMATRIX GetViewProjMatrix();
 
 	private:
+		DirectX::XMMATRIX m_viewMatrix; //ska det vara s_?
+		DirectX::XMMATRIX m_projMatrix; //ska det vara s_?
+		DirectX::XMMATRIX m_viewProjMatrix; //ska det vara s_?
 	};
 }
