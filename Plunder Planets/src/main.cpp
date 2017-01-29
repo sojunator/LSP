3ad4c92d6c088760bf60cf80a90d6e5fc93b53f9
Morgan Cromell <morgan.cromell@hotmail.com>
2017-01-29 22:22:29 +0100

    Revert "Moved the setting the the material shader into the default material constructor."
    This reverts commit d703f6144e79c0c0d8e3740b301053459cd284e1.

--- Plunder Planets/src/main.cpp
+++ Plunder Planets/src/main.cpp
@@ -14,11 +14,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 	//init code
 
 	//Init shaders
-	thomas::graphics::Shader::CreateShader("phongShader", thomas::graphics::Shader::InputLayouts::STANDARD,"../res/shaders/phong.hlsl");
+	thomas::graphics::Shader* shader = thomas::graphics::Shader::CreateShader("Phong", thomas::graphics::Shader::InputLayouts::STANDARD,
+		"../res/shaders/phong.hlsl");
 
 
 	//Init materials
-	thomas::graphics::Material::RegisterNewMaterialType("phongMaterial", new PhongMaterial("phongShader"));
+	thomas::graphics::Material::RegisterNewMaterialType("phongMaterial", new PhongMaterial());
 
 	//Init models
 
--- Plunder Planets/src/materials/PhongMaterial.h
+++ Plunder Planets/src/materials/PhongMaterial.h
@@ -8,7 +8,7 @@ class PhongMaterial : public Material
 private:
 	Material* CreateInstance(std::string dir, std::string name, aiMaterial* material);
 public:
-	PhongMaterial(std::string shaderName) : Material(shaderName) {}
+	PhongMaterial() : Material("phong") {}
 	PhongMaterial(std::string dir, std::string name, aiMaterial* material);
 
 
