95a26ff007992c88784fcb2e2df17e59fb484087
DESKTOP-4AMV6S5\Gustav <gustav.ekelund@hotmail.com>
2017-02-01 13:55:52 +0100

    changed testobject

--- Plunder Planets/src/gameobjects/TestObject.h
+++ Plunder Planets/src/gameobjects/TestObject.h
@@ -17,7 +17,6 @@ public:
 	{
 		m_renderer = AddComponent<component::RenderComponent>();
 		m_light = AddComponent<component::PointLight>();
-		m_secondLight = AddComponent<component::DirectionalLight>();
 		
 	}
 
@@ -28,9 +27,8 @@ public:
 		m_renderer->SetModel("testModel");
 		
 		m_cameraObject = Find("CameraObject");
-
-		m_light->SetDiffuseColor(thomas::math::Vector4(0, 0, 1, 1));
-		m_secondLight->SetDiffuseColor(thomas::math::Vector4(1, 0, 0, 1));
+		m_light->SetDiffuseColor(thomas::math::Vector4(0, 1, 0, 1));
+		
 		
 		//m_cameraObject->m_transform->SetPosition(math::Vector3(0, 0, 50));
 		m_transform->SetPosition(math::Vector3(0, -3, -10));
@@ -72,6 +70,5 @@ private:
 	component::RenderComponent* m_renderer;
 	GameObject* m_cameraObject;
 	component::Light* m_light;
-	component::Light* m_secondLight;
 
 };
