f8c1ac16e4d0d56bb6b316ce221423d0a8c7a44e
Albin Sahlbom <albinsahlbom@hotmail.com>
2017-02-08 14:06:43 +0100

    More islands

--- Plunder Planets/src/gameobjects/TerrainObject.h
+++ Plunder Planets/src/gameobjects/TerrainObject.h
@@ -20,7 +20,7 @@ public:
 	bool Start()
 	{
 		thomas::graphics::Material* mat = thomas::graphics::Material::CreateMaterial("terrainMat", "terrainMaterial");
-		m_islands = new thomas::Islands(1, mat, 1024 / 16, 1, 1024/2, 100);
+		m_islands = new thomas::Islands(3, mat, 1024 / 16, 1, 1024/2, 100);
 		m_model = thomas::graphics::Model::CreateModel("Plane-1", m_islands->GetIslands(0));
 		m_shipPos = math::Vector2(m_shipObject->m_transform->GetPosition().x, m_shipObject->m_transform->GetPosition().z);
 
@@ -33,6 +33,7 @@ public:
 
 
 		return true;
+
 	}
 
 
