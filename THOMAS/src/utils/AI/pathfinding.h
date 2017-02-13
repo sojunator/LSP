866125e0e412d452870eee62dfb54d78060b9b2b
Albin Sahlbom <albinsahlbom@hotmail.com>
2017-02-13 17:42:33 +0100

    Started on A*

---
+++ THOMAS/src/utils/AI/pathfinding.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "../../Common.h"
+
+namespace thomas
+{
+	namespace utils
+	{
+		namespace AI
+		{
+			class THOMAS_API Pathfinding
+			{
+			public:
+				Pathfinding();
+				~Pathfinding();
+
+			};
+		}
+	}
+}
