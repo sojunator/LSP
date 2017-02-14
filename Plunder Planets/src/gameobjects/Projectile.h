9565cd8050a58d85ad98919eb35af65bfe7fe295
sojunator <carl-pedal@hotmail.com>
2017-02-14 15:58:23 +0100

    Randomizes the Y composant for the projectiles using monteCarlo

--- Plunder Planets/src/gameobjects/Projectile.h
+++ Plunder Planets/src/gameobjects/Projectile.h
@@ -8,12 +8,14 @@ class Projectile : public GameObject
 {
 
 private:
-
+	float angleX;
+	float angleY;
 public:
 	float forwardSpeed = 0;
 	Projectile() : GameObject("Projectile")
 	{
-
+		angleX = monteCarloAngle();
+		angleY = monteCarloAngle() + 10;
 	}
 
 	void Start()
@@ -29,7 +31,8 @@ public:
 		float dt = Time::GetDeltaTime();
 		m_downSpeed += 9.82*dt;
 		m_transform->Translate(m_transform->Forward()*dt*speed);
-		m_transform->Translate(m_transform->Right()*dt*forwardSpeed/2);
+		m_transform->Translate(m_transform->Right()*dt*forwardSpeed);
+		m_transform->Translate(m_transform->Up()*dt*angleY);
 		m_transform->Translate(math::Vector3::Down*m_downSpeed*dt);
 
 
@@ -42,6 +45,15 @@ public:
 	}
 
 private:
+	float monteCarloAngle()
+	{
+		float randNum = rand() % 999 + 1;
+		randNum /= 1000.0f;
+		if (randNum < 0.5f)
+			return 15 * (sqrtf(2.0f * randNum) - 1);
+		else
+			return 5 * (1 - sqrtf(2 - 2 * randNum));
+	}
 	float m_downSpeed;
 	component::SoundComponent* m_splashSound;
 	component::RenderComponent* m_renderer;
