763753872433ce419941b709d46a800bb580ac1c
Albin Sahlbom <albinsahlbom@hotmail.com>
2017-02-14 15:38:43 +0100

    Started on movement

--- Plunder Planets/src/gameobjects/Enemy.h
+++ Plunder Planets/src/gameobjects/Enemy.h
@@ -2,6 +2,7 @@
 #include <Thomas.h>
 #include "Broadside.h"
 #include "TerrainObject.h"
+#include "Ship.h"
 
 namespace thomas
 {
@@ -20,6 +21,7 @@ namespace thomas
 				m_renderer = AddComponent<component::RenderComponent>();
 				m_sound = AddComponent<component::SoundComponent>();
 				m_terrainObject = (TerrainObject*)Find("TerrainObject");
+				m_ship = (Ship*)Find("Ship");
 
 				/*
 				m_broadSideLeft = Instantiate<Broadside>(math::Vector3(-3, 3, -0.8), math::Quaternion::CreateFromAxisAngle(math::Vector3(0, 1, 0), math::PI / 2), m_transform, m_scene);
@@ -34,16 +36,62 @@ namespace thomas
 				m_acceleration = 9.5f;
 				m_retardation = 6.5f;
 				m_maxSpeed = 30.0f;
+				m_rotation = 0.3f;
 
+				m_escapeTime = 100;		//Should be 10 seconds
+				m_escapeTimer = 0;
+
+				m_searchRadius = 1000;
 				m_dt = 0;
 			}
 
 			void Move()
 			{
-
+				if (math::Vector3::DistanceSquared(m_playerPos, m_transform->GetPosition()) < 100)
+				{
+					m_forwardSpeed -= m_retardation * m_dt;
+					m_forwardSpeed = std::fmaxf(m_forwardSpeed, 0);
+				}
+				else
+				{
+					m_forwardSpeed += m_acceleration * m_dt;
+					m_forwardSpeed = std::fminf(m_forwardSpeed, m_maxSpeed);
+				}
 			}
 
 			void Rotate()
+			{
+				
+			}
+
+			void InsideRadius()
+			{
+				float distanceSquared = math::Vector3::DistanceSquared(m_playerPos, m_transform->GetPosition());
+				if (distanceSquared <= (m_searchRadius * m_searchRadius))
+				{
+					m_escapeTimer = 0;
+					m_newForwardVec = m_playerPos - m_transform->GetPosition();
+					m_state = Attacking;
+				}
+				else if (m_state == Attacking)
+				{
+					m_lastKnownPos = m_playerPos;
+					m_escapeTimer += m_dt;
+					m_state = Searching;
+				}
+				else
+				{
+					m_escapeTimer += m_dt;
+				}
+			}
+
+			void Escaped()
+			{
+				if (m_escapeTimer >= m_escapeTime)
+					m_state = Idle;
+			}
+
+			void FireCannons()
 			{
 
 			}
@@ -51,17 +99,33 @@ namespace thomas
 			void Update()
 			{
 				m_dt = Time::GetDeltaTime();
+				m_playerPos = m_ship->m_transform->GetPosition();
+				InsideRadius();
 
+				switch (m_state)
+				{
+				case Searching:
+				{
+					break;
+				}
+				case Idle:
+				{
+					break;
+				}
+				case Attacking:
+				{
+					break;
+				}
+				default:
+					break;
+				}
 				
 
 			}
 
 		private:
-
-		public:
 			//Used for ship
 			math::Vector3 m_initPosition;
-			math::Vector3 m_forwardVec;
 			math::Vector3 m_newForwardVec;
 			float m_forwardSpeed;
 			float m_newForward;
@@ -69,8 +133,23 @@ namespace thomas
 			float m_acceleration;
 			float m_retardation;
 			float m_maxSpeed;
+			float m_searchRadius;
+
+			float m_escapeTimer;
+			float m_escapeTime;
+
+			math::Vector3 m_playerPos;
+			math::Vector3 m_lastKnownPos;
 
 			//Behaviors
+			enum Behavior
+			{
+				Searching,
+				Idle,
+				Attacking,
+			};
+			
+			Behavior m_state = Idle;
 
 			float m_dt;
 
@@ -83,6 +162,7 @@ namespace thomas
 			TerrainObject* m_terrainObject;
 			Broadside* m_broadSideLeft;
 			Broadside* m_broadSideRight;
+			Ship* m_ship;
 
 		};
 	}
