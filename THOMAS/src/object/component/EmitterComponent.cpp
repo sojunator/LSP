08cfaa36c78a847674fdb5caaf27c1f0167f24cd
Morgan Cromell <morgan.cromell@hotmail.com>
2017-02-20 21:55:18 +0100

    Changed name of emitter component. Moved into correct namespace format.

--- THOMAS/src/object/component/EmitterComponent.cpp
+++ THOMAS/src/object/component/EmitterComponent.cpp
@@ -1,45 +1,54 @@
 #include "EmitterComponent.h"
 
-
-thomas::object::component::EmitterComponent::EmitterComponent() : Component("EmitterComponent")
+namespace thomas
 {
-	m_nrOfParticles = 5832;
-	m_isEmitting = true;
+	namespace object
+	{
+		namespace component
+		{
+			ParticleEmitterComponent::ParticleEmitterComponent() : Component("ParticleEmitterComponent")
+			{
+				m_nrOfParticles = 5832;
+				m_isEmitting = true;
 
-	m_particleD3D = new thomas::graphics::ParticleSystem::ParticleD3D();
+				m_particleD3D = new thomas::graphics::ParticleSystem::ParticleD3D();
 
-	m_particleD3D->m_shader = graphics::Shader::GetShaderByName("particleShader");
-	m_particleD3D->m_texture = graphics::Texture::CreateTexture(thomas::graphics::Texture::SamplerState::WRAP, thomas::graphics::Texture::TextureType::DIFFUSE, "../res/textures/smokeParticleDank.png");
-	m_particleD3D->m_booleanSwapUAVandSRV = true;
+				m_particleD3D->m_shader = graphics::Shader::GetShaderByName("particleShader");
+				m_particleD3D->m_texture = graphics::Texture::CreateTexture(thomas::graphics::Texture::SamplerState::WRAP, thomas::graphics::Texture::TextureType::DIFFUSE, "../res/textures/smokeParticleDank.png");
+				m_particleD3D->m_booleanSwapUAVandSRV = true;
 
-	graphics::ParticleSystem::AddEmitter(this);
+				graphics::ParticleSystem::AddEmitter(this);
+			}
+
+			void ParticleEmitterComponent::Emit()
+			{
+				m_isEmitting = true;
+
+			}
+
+			bool ParticleEmitterComponent::IsEmitting() const
+			{
+				return m_isEmitting;
+			}
+
+
+			void ParticleEmitterComponent::Stop()
+			{
+				m_isEmitting = false;
+			}
+
+			unsigned int ParticleEmitterComponent::GetNrOfParticles() const
+			{
+				return m_nrOfParticles;
+			}
+
+
+
+			graphics::ParticleSystem::ParticleD3D* ParticleEmitterComponent::GetParticleD3D() const
+			{
+				return m_particleD3D;
+			}
+		}
+	}
 }
 
-void thomas::object::component::EmitterComponent::Emit()
-{
-	m_isEmitting = true;
-
-}
-
-bool thomas::object::component::EmitterComponent::IsEmitting() const
-{
-	return m_isEmitting;
-}
-
-
-void thomas::object::component::EmitterComponent::Stop()
-{
-	m_isEmitting = false;
-}
-
-unsigned int thomas::object::component::EmitterComponent::GetNrOfParticles() const
-{
-	return m_nrOfParticles;
-}
-
-
-
-thomas::graphics::ParticleSystem::ParticleD3D* thomas::object::component::EmitterComponent::GetParticleD3D() const
-{
-	return m_particleD3D;
-}
