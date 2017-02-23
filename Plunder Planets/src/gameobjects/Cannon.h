8b8f3c8a27ae96948de622aac47ffa57232814df
DESKTOP-4AMV6S5\Gustav <gustav.ekelund@hotmail.com>
2017-02-23 14:08:21 +0100

    Added a lame muzzleflash

--- Plunder Planets/src/gameobjects/Cannon.h
+++ Plunder Planets/src/gameobjects/Cannon.h
@@ -20,8 +20,11 @@ public:
 
 	void Start()
 	{
-		m_emitterComponent = AddComponent<component::EmitterComponent>();
-		m_emitterComponent->Init(256 * 10 + 254, false, math::Vector3(0, 1, 0), 0.0f, 0.0f, 2.0f, 6.4f, m_transform->GetPosition(), 0.035f, 0.6f, 2.2f, 1.4f, 3.2f, "particleShader", "../res/textures/smoke.dds");
+		m_emitterSmoke = AddComponent<component::EmitterComponent>();
+		m_emitterSmoke->Init(256 * 10 + 254, false, math::Vector3(0, 1, 0), 0.3f, 0.3f, 2.0f, 6.4f, m_transform->GetPosition(), 0.035f, 0.6f, 2.2f, 1.4f, 3.2f, "particleShader", "../res/textures/smoke.dds");
+
+		m_emitterSpark = AddComponent<component::EmitterComponent>();
+		m_emitterSpark->Init(126, false, math::Vector3(0, 0, 1), 0.0f, 0.0f, 26.0f, 38.4f, m_transform->GetPosition(), 0.3f, 0.2f, 0.4f, 0.06f, 0.08f, "particleShader", "../res/textures/spark.png");
 
 		roof = 0.8f;
 		ReseedDelay();
@@ -45,8 +48,10 @@ public:
 			{
 				math::Vector3 smokeDir = m_transform->Forward() * 0.5 + math::Vector3(0, 1, 0);
 				smokeDir.Normalize();
-				m_emitterComponent->Update(NULL, smokeDir, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-				m_emitterComponent->Emit();
+				m_emitterSmoke->Update(NULL, smokeDir, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+				m_emitterSmoke->Emit();
+				m_emitterSpark->Update(NULL, m_transform->Forward(), NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+				m_emitterSpark->Emit();
 				// instanciate projectile
 				Projectile* p = Instantiate<Projectile>(m_transform->GetPosition(), m_transform->GetRotation(), m_scene);
 				p->m_spawnedBy = m_transform->GetParent()->GetParent()->m_gameObject;
@@ -79,5 +84,6 @@ private:
 	float delay;
 	float roof;
 	float currentTimeCount;
-	component::EmitterComponent* m_emitterComponent;
+	component::EmitterComponent* m_emitterSmoke;
+	component::EmitterComponent* m_emitterSpark;
 };
