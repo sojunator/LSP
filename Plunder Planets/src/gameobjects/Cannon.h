a9ad98b0893417004c6ebb5a4e67d2c413513108
DESKTOP-4AMV6S5\Gustav <gustav.ekelund@hotmail.com>
2017-03-03 14:44:16 +0100

    smooooooke

--- Plunder Planets/src/gameobjects/Cannon.h
+++ Plunder Planets/src/gameobjects/Cannon.h
@@ -23,44 +23,44 @@ public:
 		m_emitterSmoke->SetShader("particleShader");
 		m_emitterSmoke->SetDirection(math::Vector3(0, 1, 0));
 		m_emitterSmoke->SetStartColor(math::Vector4(1.0f, 1.0f, 1.0f, 1.0f));
-		m_emitterSmoke->SetEndColor(math::Vector4(1.0f, 1.0f, 1.0f, 0.4f));
-		m_emitterSmoke->SetMaxDelay(0.75f);
-		m_emitterSmoke->SetMinDelay(0.15f);
+		m_emitterSmoke->SetEndColor(math::Vector4(1.0f, 1.0f, 1.0f, 0.5f));
+		m_emitterSmoke->SetMaxDelay(0.25f);
+		m_emitterSmoke->SetMinDelay(0.05f);
 		m_emitterSmoke->SetMaxSpeed(6.0f);
 		m_emitterSmoke->SetMinSpeed(3.0f);
-		m_emitterSmoke->SetMaxSize(2.4f);
-		m_emitterSmoke->SetMinSize(1.4f);
-		m_emitterSmoke->SetEndSize(3.4f);
-		m_emitterSmoke->SetMaxLifeTime(1.85f);
+		m_emitterSmoke->SetMaxSize(1.7f);
+		m_emitterSmoke->SetMinSize(1.0f);
+		m_emitterSmoke->SetEndSize(2.4f);
+		m_emitterSmoke->SetMaxLifeTime(1.2f);
 		m_emitterSmoke->SetMinLifeTime(0.7f);
-		m_emitterSmoke->SetRotationSpeed(1.0f);
+		m_emitterSmoke->SetRotationSpeed(2.5f);
 		m_emitterSmoke->SetSpread(1.44f);
 		m_emitterSmoke->SetEmissionRate(250);
-		m_emitterSmoke->SetEmissionDuration(1.2f);
+		m_emitterSmoke->SetEmissionDuration(0.4f);
 		
 		m_emitterSmoke2 = AddComponent<component::ParticleEmitterComponent>();
 		m_emitterSmoke2->SetTexture("../res/textures/smokethick.png");
 		m_emitterSmoke2->SetShader("particleShader");
 		m_emitterSmoke2->SetDirection(math::Vector3(0, 1, 0));
 		m_emitterSmoke2->SetStartColor(math::Vector4(1.0f, 1.0f, 1.0f, 1.0f));
-		m_emitterSmoke2->SetEndColor(math::Vector4(1.0f, 1.0f, 1.0f, 0.4f));
-		m_emitterSmoke2->SetMaxDelay(0.5f);
-		m_emitterSmoke2->SetMinDelay(0.15f);
-		m_emitterSmoke2->SetMaxSpeed(9.0f);
-		m_emitterSmoke2->SetMinSpeed(4.0f);
+		m_emitterSmoke2->SetEndColor(math::Vector4(0.0f, 0.0f, 0.0f, 0.1f));
+		m_emitterSmoke2->SetMaxDelay(0.15f);
+		m_emitterSmoke2->SetMinDelay(0.01f);
+		m_emitterSmoke2->SetMaxSpeed(14.0f);
+		m_emitterSmoke2->SetMinSpeed(8.0f);
 		m_emitterSmoke2->SetMaxSize(1.0f);
-		m_emitterSmoke2->SetMinSize(0.5f);
-		m_emitterSmoke2->SetEndSize(1.7);
-		m_emitterSmoke2->SetMaxLifeTime(1.7f);
+		m_emitterSmoke2->SetMinSize(0.7f);
+		m_emitterSmoke2->SetEndSize(0);
+		m_emitterSmoke2->SetMaxLifeTime(1.0f);
 		m_emitterSmoke2->SetMinLifeTime(0.5f);
-		m_emitterSmoke2->SetSpread(1.24f);
-		m_emitterSmoke2->SetEmissionRate(390);
-		m_emitterSmoke2->SetEmissionDuration(1.2f);
+		m_emitterSmoke2->SetSpread(1.0f);
+		m_emitterSmoke2->SetEmissionRate(200);
+		m_emitterSmoke2->SetEmissionDuration(0.4f);
 		m_emitterSmoke2->SetRotationSpeed(1.0f);
 
 		m_emitterSpark = AddComponent<component::ParticleEmitterComponent>();
 
-		m_emitterSpark->SetTexture("../res/textures/fire.png");
+		m_emitterSpark->SetTexture("../res/textures/spark.png");
 		m_emitterSpark->SetShader("particleShader");
 		m_emitterSpark->SetMaxDelay(0.0f);
 		m_emitterSpark->SetMinDelay(0.0f);
@@ -95,7 +95,7 @@ public:
 		{
 			if (m_monteCarloDelay < 0)
 			{
-				math::Vector3 dir = m_transform->Forward();
+				math::Vector3 dir = m_transform->Forward() * 3 + m_transform->Up();
 				dir.Normalize();
 				m_emitterSmoke->SetDirection(dir);
 				m_emitterSmoke->StartEmitting();
