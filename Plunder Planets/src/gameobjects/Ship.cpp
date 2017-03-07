1deabc3f3ce5016f8ebe6582eba7a9ea98066ab8
steve-roper <oskar.warnhag@gmail.com>
2017-03-07 16:56:43 +0100

    Added visual effects for costs of boost and firing

--- Plunder Planets/src/gameobjects/Ship.cpp
+++ Plunder Planets/src/gameobjects/Ship.cpp
@@ -128,6 +128,44 @@ void Ship::Start()
 	m_boosterParticlesEmitterRight2->SetRotationSpeed(2.0f);
 	m_boosterParticlesEmitterRight2->SetSpread(2.5f);
 
+	//Fire cost
+	m_firingCost = AddComponent<component::ParticleEmitterComponent>();
+	m_firingCost->SetTexture("../res/textures/FiringCost" + std::to_string((int)ShipStats::s_playerStats->GetCannonCost()) + ".png");
+	m_firingCost->SetShader("particleShader");
+	//m_firingCost->SetLooping(false);
+	m_firingCost->SetDirection(0, 1, 0);
+	m_firingCost->SetSpread(0);
+	m_firingCost->SetSpeed(25);
+	//m_firingCost->SetEndSpeed(200);
+	//m_firingCost->SetRotationSpeed(11);
+	m_firingCost->SetLifeTime(1.5);
+	m_firingCost->SetSize(3);
+	m_firingCost->SetEmissionRate(1);
+	m_firingCost->SetEmissionDuration(1);
+	m_firingCost->SetEndSize(3);
+	//m_firingCost->SetRadius(70);
+	//m_firingCost->SetEndColor(math::Color(0, 1, 0, 1));
+	//m_firingCost->SpawnAtSphereEdge(true);
+
+
+	m_boostCost = AddComponent<component::ParticleEmitterComponent>();
+	m_boostCost->SetTexture("../res/textures/BoostCost" + std::to_string((int)ShipStats::s_playerStats->GetBoostCost()) + ".png");
+	m_boostCost->SetShader("particleShader");
+	//m_firingCost->SetLooping(false);
+	m_boostCost->SetDirection(0, 1, 0);
+	m_boostCost->SetSpread(0);
+	m_boostCost->SetSpeed(25);
+	//m_firingCost->SetEndSpeed(200);
+	//m_firingCost->SetRotationSpeed(11);
+	m_boostCost->SetLifeTime(1.5);
+	m_boostCost->SetSize(3);
+	m_boostCost->SetEmissionRate(1);
+	m_boostCost->SetEmissionDuration(1);
+	m_boostCost->SetEndSize(3);
+	//m_firingCost->SetRadius(70);
+	//m_firingCost->SetEndColor(math::Color(0, 1, 0, 1));
+	//m_firingCost->SpawnAtSphereEdge(true);
+
 	thomas::graphics::TextRender::LoadFont("SafeToLeave", "../res/font/pirate.spritefont");
 
 	m_safeToLeave = AddComponent<component::TextComponent>();
@@ -325,6 +363,7 @@ void Ship::ShipAimCannons()
 			Input::Vibrate(0.0, 0.5, 0.5);
 			m_broadSideLeft->Fire(); //Temporary fix
 			m_treasure -= ShipStats::s_playerStats->GetCannonCost();
+			m_firingCost->StartEmitting();
 		}
 	}
 
@@ -355,6 +394,7 @@ void Ship::ShipAimCannons()
 			m_treasure -= ShipStats::s_playerStats->GetCannonCost();
 			Input::Vibrate(0.5, 0, 0.5);
 			m_broadSideRight->Fire(); //Temporary fix
+			m_firingCost->StartEmitting();
 		}
 			
 
@@ -626,6 +666,8 @@ void Ship::Update()
 		m_boosterParticlesEmitterRight2->SetOffset(m_transform->Forward() * 10.45f + m_transform->Up() * 3.25f + m_transform->Right() * -6.66f);
 		m_boosterParticlesEmitterRight2->SetDirection(m_transform->Forward());
 		m_boosterParticlesEmitterRight2->StartEmitting();
+
+		m_boostCost->StartEmitting();
 		
 		m_boostSound->Play();
 	}
@@ -642,7 +684,7 @@ void Ship::Update()
 	Float(dt);
 
 
-	if (m_treasure > 500 && !m_spawnedWormhole && !m_startUpSequence)
+	if (m_treasure > 100000 && !m_spawnedWormhole && !m_startUpSequence)
 	{
 		Wormhole* wormhole = Instantiate<Wormhole>(math::Vector3(0,3.0f,0),math::Quaternion::Identity,m_scene);
 		wormhole->SetEndLevel(true);
