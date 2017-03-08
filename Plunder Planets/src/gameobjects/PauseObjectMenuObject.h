548205524dcc1b96ab25fbdedf1011ade71774ba
NickWedzinga <oxay@live.com>
2017-03-08 13:51:29 +0100

    Added settings funcionality in Pause menu.

--- Plunder Planets/src/gameobjects/PauseObjectMenuObject.h
+++ Plunder Planets/src/gameobjects/PauseObjectMenuObject.h
@@ -21,8 +21,10 @@ public:
 	{
 		m_isPaused = false;
 		m_pauseHeadLine = AddComponent<component::TextComponent>();
-		m_pauseQuit = AddComponent<component::TextComponent>();
 		m_pauseResume = AddComponent<component::TextComponent>();
+		m_pauseSettings = AddComponent<component::TextComponent>();
+		m_pauseQuit = AddComponent<component::TextComponent>();
+		
 
 		InitMenu();
 	}
@@ -51,13 +53,24 @@ public:
 		m_pauseResume->SetOutline(true);
 		m_pauseResume->SetOrigin(false);
 
+		m_pauseSettings->SetFont("SafeToLeave");
+		m_pauseSettings->SetOutput("Settings");
+		m_pauseSettings->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
+		m_pauseSettings->SetRotation(0.0f);
+		m_pauseSettings->SetScale(1.0f);
+		m_pauseSettings->SetPositionX(Window::GetWidth() / 4);
+		m_pauseSettings->SetPositionY(Window::GetHeight() / 4 + 200);
+		m_pauseSettings->SetDropshadow(true);
+		m_pauseSettings->SetOutline(true);
+		m_pauseSettings->SetOrigin(false);
+
 		m_pauseQuit->SetFont("SafeToLeave");
 		m_pauseQuit->SetOutput("Quit");
 		m_pauseQuit->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
 		m_pauseQuit->SetRotation(0.0f);
 		m_pauseQuit->SetScale(1.0f);
 		m_pauseQuit->SetPositionX(Window::GetWidth() / 4);
-		m_pauseQuit->SetPositionY(Window::GetHeight() / 4 + 200 );
+		m_pauseQuit->SetPositionY(Window::GetHeight() / 4 + 300 );
 		m_pauseQuit->SetDropshadow(true);
 		m_pauseQuit->SetOutline(true);
 		m_pauseQuit->SetOrigin(false);
@@ -72,15 +85,18 @@ public:
 	void DisplayMenu()
 	{
 		m_pauseHeadLine->SetActive(true);
-		m_pauseQuit->SetActive(true);
 		m_pauseResume->SetActive(true);
+		m_pauseSettings->SetActive(true);
+		m_pauseQuit->SetActive(true);
+		
 	}
 
 	void HideMenu()
 	{
-		m_pauseHeadLine->SetActive(false); 
-		m_pauseQuit->SetActive(false);
+		m_pauseHeadLine->SetActive(false); 		
 		m_pauseResume->SetActive(false);
+		m_pauseSettings->SetActive(false);
+		m_pauseQuit->SetActive(false);
 	}
 
 	void CheckState()
@@ -107,32 +123,49 @@ public:
 		{
 			if (m_isPaused)
 			{
-				if (Input::GetButton(Input::Buttons::DPAD_DOWN) || Input::GetLeftStickY() > 0.0f || Input::GetKey(Input::Keys::Down))
+				if (Input::GetButton(Input::Buttons::DPAD_DOWN) || Input::GetLeftStickY() < 0.0f || Input::GetKey(Input::Keys::Down))
 				{
 					if (m_resumeActive)
 					{
-						m_quitActive = true;
 						m_resumeActive = false;
+						m_settingsActive = true;
+						m_quitActive = false;
 					}
-					else
+					else if (m_settingsActive)
+					{
+						m_resumeActive = false;
+						m_settingsActive = false;
+						m_quitActive = true;
+					}
+					else if(m_quitActive)
 					{
 						m_resumeActive = true;
+						m_settingsActive = false;
 						m_quitActive = false;
 					}
 
 					m_inputDelay = 0.3f;
 				}
 
-				if (Input::GetButton(Input::Buttons::DPAD_UP) || Input::GetLeftStickY() < 0.0f || Input::GetKey(Input::Keys::Up))
+				if (Input::GetButton(Input::Buttons::DPAD_UP) || Input::GetLeftStickY() > 0.0f || Input::GetKey(Input::Keys::Up))
 				{
 					if (m_resumeActive)
 					{
-						m_quitActive = true;
 						m_resumeActive = false;
+						m_settingsActive = false;
+						m_quitActive = true;
+						
 					}
-					else
+					else if (m_settingsActive)
 					{
 						m_resumeActive = true;
+						m_settingsActive = false;
+						m_quitActive = false;
+					}
+					else if (m_quitActive)
+					{
+						m_resumeActive = false;
+						m_settingsActive = true;
 						m_quitActive = false;
 					}
 
@@ -151,11 +184,19 @@ public:
 		if (m_resumeActive)
 		{
 			m_pauseResume->SetColor(math::Vector3(1.0f, 0.0f, 0.0f));
+			m_pauseSettings->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
 			m_pauseQuit->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
 		}
-		else
+		else if(m_settingsActive)
 		{
 			m_pauseResume->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
+			m_pauseSettings->SetColor(math::Vector3(1.0f, 0.0f, 0.0f));
+			m_pauseQuit->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
+		}
+		else if(m_quitActive)
+		{
+			m_pauseResume->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
+			m_pauseSettings->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
 			m_pauseQuit->SetColor(math::Vector3(1.0f, 0.0f, 0.0f));
 		}
 	}
@@ -174,6 +215,11 @@ public:
 			{
 				m_isPaused = false;
 				HideMenu();
+			}
+
+			if (m_settingsActive)
+			{
+				//soon^tm
 			}
 		}
 	}
@@ -202,13 +248,21 @@ public:
 		}
 	}
 
+	bool GetSettingsActive()
+	{
+		return m_settingsActive;
+	}
+
 
 private:
 	float m_inputDelay;
 	bool m_isPaused;
 	bool m_resumeActive;
+	bool m_settingsActive;
 	bool m_quitActive;
 	component::TextComponent* m_pauseHeadLine;
-	component::TextComponent* m_pauseQuit;
 	component::TextComponent* m_pauseResume;
+	component::TextComponent* m_pauseSettings;
+	component::TextComponent* m_pauseQuit;
+	
 };
---
+++ Plunder Planets/src/gameobjects/SettingsMenuObject.h
@@ -0,0 +1,203 @@
+#pragma once
+#pragma once
+#include <Thomas.h>
+#include "PauseObjectMenuObject.h"
+
+using namespace thomas;
+using namespace object;
+class SettingsMenuObject : public GameObject
+{
+
+private:
+
+public:
+	SettingsMenuObject() : GameObject("SettingsMenuObject")
+	{
+
+	}
+	~SettingsMenuObject()
+	{
+	}
+
+	void Start()
+	{
+		m_settingsActive = false;
+		m_settingsHeadLine = AddComponent<component::TextComponent>();
+		/*m_pauseQuit = AddComponent<component::TextComponent>();
+		m_pauseResume = AddComponent<component::TextComponent>();*/
+
+		InitMenu();
+	}
+
+	void InitMenu()
+	{
+		m_settingsHeadLine->SetFont("SafeToLeave");
+		m_settingsHeadLine->SetOutput("Settings");
+		m_settingsHeadLine->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
+		m_settingsHeadLine->SetRotation(0.0f);
+		m_settingsHeadLine->SetScale(2.0f);
+		m_settingsHeadLine->SetPositionX((Window::GetWidth() / 4) * 3);
+		m_settingsHeadLine->SetPositionY(Window::GetHeight() / 4.5);
+		m_settingsHeadLine->SetDropshadow(true);
+		m_settingsHeadLine->SetOutline(true);
+		m_settingsHeadLine->SetOrigin(false);
+
+		/*m_pauseResume->SetFont("SafeToLeave");
+		m_pauseResume->SetOutput("Resume");
+		m_pauseResume->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
+		m_pauseResume->SetRotation(0.0f);
+		m_pauseResume->SetScale(1.0f);
+		m_pauseResume->SetPositionX(Window::GetWidth() / 4);
+		m_pauseResume->SetPositionY(Window::GetHeight() / 4 + 100);
+		m_pauseResume->SetDropshadow(true);
+		m_pauseResume->SetOutline(true);
+		m_pauseResume->SetOrigin(false);
+
+		m_pauseQuit->SetFont("SafeToLeave");
+		m_pauseQuit->SetOutput("Quit");
+		m_pauseQuit->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
+		m_pauseQuit->SetRotation(0.0f);
+		m_pauseQuit->SetScale(1.0f);
+		m_pauseQuit->SetPositionX(Window::GetWidth() / 4);
+		m_pauseQuit->SetPositionY(Window::GetHeight() / 4 + 200);
+		m_pauseQuit->SetDropshadow(true);
+		m_pauseQuit->SetOutline(true);
+		m_pauseQuit->SetOrigin(false);
+
+		m_resumeActive = true;
+		m_quitActive = false;*/
+
+		//m_inputDelay = 0.0f; // so that we don't do 500 inputs per second.
+		//HideMenu();
+	}
+
+	void DisplayMenu()
+	{
+		/*m_settingsHeadLine->SetActive(true);
+		m_pauseQuit->SetActive(true);
+		m_pauseResume->SetActive(true);*/
+	}
+
+	void HideMenu()
+	{
+		/*m_settingsHeadLine->SetActive(false);
+		m_pauseQuit->SetActive(false);
+		m_pauseResume->SetActive(false);*/
+	}
+
+	void CheckState()
+	{
+		/*if (Input::GetButtonDown(Input::Buttons::START) || Input::GetKeyDown(Input::Keys::Enter))
+		{
+
+			if (m_isPaused)
+			{
+				HideMenu();
+				m_isPaused = false;
+			}
+			else
+			{
+				DisplayMenu();
+				m_isPaused = true;
+			}
+		}*/
+	}
+
+	void CheckInput()
+	{/*
+		if (m_inputDelay < 0.001f)
+		{
+			if (m_isPaused)
+			{
+				if (Input::GetButton(Input::Buttons::DPAD_DOWN) || Input::GetLeftStickY() > 0.0f || Input::GetKey(Input::Keys::Down))
+				{
+					if (m_resumeActive)
+					{
+						m_quitActive = true;
+						m_resumeActive = false;
+					}
+					else
+					{
+						m_resumeActive = true;
+						m_quitActive = false;
+					}
+
+					m_inputDelay = 0.3f;
+				}
+
+				if (Input::GetButton(Input::Buttons::DPAD_UP) || Input::GetLeftStickY() < 0.0f || Input::GetKey(Input::Keys::Up))
+				{
+					if (m_resumeActive)
+					{
+						m_quitActive = true;
+						m_resumeActive = false;
+					}
+					else
+					{
+						m_resumeActive = true;
+						m_quitActive = false;
+					}
+
+					m_inputDelay = 0.3f;
+				}
+			}*/
+		/*}
+		else
+		{
+			m_inputDelay -= thomas::ThomasTime::GetActualDeltaTime();
+		}*/
+	}
+
+	void SetTextActive()
+	{
+		/*if (m_resumeActive)
+		{
+			m_pauseResume->SetColor(math::Vector3(1.0f, 0.0f, 0.0f));
+			m_pauseQuit->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
+		}
+		else
+		{
+			m_pauseResume->SetColor(math::Vector3(1.0f, 1.0f, 0.0f));
+			m_pauseQuit->SetColor(math::Vector3(1.0f, 0.0f, 0.0f));
+		}*/
+	}
+
+	void Choice()
+	{
+		if (Input::GetButtonDown(Input::Buttons::A) || Input::GetKeyDown(Input::Keys::Space))
+		{
+			/*if (m_quitActive)
+			{
+				thomas::Scene::UnloadScene();
+				Scene::LoadScene<MenuScene>();
+			}
+
+			if (m_resumeActive)
+			{
+				m_isPaused = false;
+				HideMenu();
+			}*/
+		}
+	}
+
+	void Update()
+	{
+		/*CheckState();
+		CheckInput();
+
+		if (m_isPaused)
+		{
+			SetTextActive();
+			Choice();
+		}*/
+	};
+
+private:
+	//float m_inputDelay;
+	bool m_settingsActive;
+	//bool m_resumeActive;
+	//bool m_quitActive;
+	component::TextComponent* m_settingsHeadLine;
+	//component::TextComponent* m_pauseQuit;
+	//component::TextComponent* m_pauseResume;
+};
