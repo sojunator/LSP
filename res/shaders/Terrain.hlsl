e4a5b4bc8133c2a4891b56b0648330b4d0ccfc3c
DESKTOP-4AMV6S5\Gustav <gustav.ekelund@hotmail.com>
2017-03-10 13:39:47 +0100

    minor tweak to particles, changed some shading

--- res/shaders/Terrain.hlsl
+++ res/shaders/Terrain.hlsl
@@ -140,6 +140,17 @@ float4 PSMain(VSOutput input) : SV_TARGET
         float4 diffuse = saturate((diffuseColor + textureColor) * lightIntensity) * directionalLights[i].lightColor;
         outputColor += diffuse;
     }
-    return outputColor + ambientColor;
-    return float4((outputColor + ambientColor).rgb, 1);
+    float4 outputTotal = outputColor + ambientColor;
+    float4 unShadedOutput = float4(textureColor.xyz * 0.5f, 1);
+    if (input.positionWS.y > 0.0f)
+        return outputTotal;
+
+    if (input.positionWS.y > -30.0f)
+    {
+        float heightLerpValue = (30 + input.positionWS.y) * 0.1;
+        float heightLerpValueInv = 1 - heightLerpValue;
+        return outputTotal * heightLerpValue + unShadedOutput * heightLerpValueInv;
+    }
+    
+    return unShadedOutput;  
 }
